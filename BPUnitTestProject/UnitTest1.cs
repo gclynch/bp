4b03ef6963f151f0aa7e1b70a81106853c858289
RakulTUD <125701931+RakulTUD@users.noreply.github.com>
2023-12-19 00:16:56 +0530

    Update UnitTest1.cs

--- BPUnitTestProject/UnitTest1.cs
+++ BPUnitTestProject/UnitTest1.cs
@@ -15,11 +15,67 @@ namespace BPUnitTestProject
            Assert.AreEqual(1, 1);
         }
 
-        // [TestMethod]
-        // public void TestMethod2()
-        // {
-        //     BMI bmi = new BMI() { WeightStones = 15, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
-        //     Assert.AreEqual(bmi.BMICategory, BMICategory.Obese);
-        // }
+        [TestMethod]
+        public void TestMethod10()
+        {
+           BloodPressure bp =  new BloodPressure() {Systolic =71 , Diastolic =41 };
+           Assert.AreEqual(bp.Category, BPCategory.LowDiastolic);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+           BloodPressure bp =  new BloodPressure() {Systolic = 91, Diastolic = 61};
+           Assert.AreEqual(bp.Category, BPCategory.IdealDiastolic);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+           BloodPressure bp =  new BloodPressure() {Systolic =121 , Diastolic =81 };
+           Assert.AreEqual(bp.Category, BPCategory.PreHighDiastolic);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+           BloodPressure bp =  new BloodPressure() {Systolic =141 , Diastolic =91 };
+           Assert.AreEqual(bp.Category, BPCategory.HighDiastolic);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+           BloodPressure bp =  new BloodPressure() {Systolic = 81, Diastolic =41 };
+           Assert.AreEqual(bp.Category, BPCategory.Low);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+           BloodPressure bp =  new BloodPressure() {Systolic =91 , Diastolic = 61};
+           Assert.AreEqual(bp.Category, BPCategory.Ideal);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+           BloodPressure bp =  new BloodPressure() {Systolic =121 , Diastolic = 81};
+           Assert.AreEqual(bp.Category, BPCategory.PreHigh);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+           BloodPressure bp =  new BloodPressure() {Systolic =141 , Diastolic = 91};
+           Assert.AreEqual(bp.Category, BPCategory.High);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+           BloodPressure bp =  new BloodPressure() {Systolic = 40, Diastolic = 20};
+           Assert.AreEqual(bp.Category, BPCategory.Empty);
+        }
     }
 }
